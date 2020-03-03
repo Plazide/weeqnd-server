978f8871049b20316f300fdd69ec6e3e3be97c05
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-03 12:47:41 +0100

    Update refresh function

--- actions/refresh.js
+++ actions/refresh.js
@@ -5,28 +5,50 @@ const gql = require("graphql-tag");
  * Refresh access token of provided party.
  */
 async function refresh (){
-	const result = await this.refreshAccessToken().catch( err => err);
-	const accessToken = result.body["access_token"];
-	const refreshToken = result.body["refresh_token"] || this.refreshToken;
+	try{
+		console.log("Refreshing access token...");
 
-	const updateAccessToken = gql`
-		mutation {
-			partialUpdateParty(id: "${this._id}", data: {
-				accessToken: "${accessToken}"
-				refreshToken: "${refreshToken}"
-			}){
-				accessToken
-				refreshToken
-				owner
+		const result = await this.refreshAccessToken().catch( err => {
+			console.error(err);
+
+			return err;
+		});
+
+		const accessToken = result.body["access_token"];
+		const refreshToken = result.body["refresh_token"] || this.getRefreshToken();
+
+		this.setAccessToken(accessToken);
+		this.setRefreshToken(refreshToken);
+
+		const updateAccessToken = gql`
+			mutation {
+				partialUpdateParty(id: "${this._id}", data: {
+					accessToken: "${accessToken}"
+					refreshToken: "${refreshToken}"
+				}){
+					accessToken
+					refreshToken
+					owner
+				}
 			}
-		}
-		`;
+			`;
 
-	const update = await client.mutate({ mutation: updateAccessToken });
+		await client.mutate({ mutation: updateAccessToken });
 
-	this.setAccessToken(update.data.partialUpdateParty.accessToken);
-	this.setRefreshToken(update.data.partialUpdateParty.refreshToken);
-	return true;
+		this.io.clients( (err, clients) => {
+			if(err) throw err;
+
+			const ownerSocket = clients.find( client => client.username === this.owner);
+			ownerSocket.emit("token_refreshed", { accessToken });
+		});
+
+		console.log("Access token refreshed!");
+		return true;
+	}catch(err){
+		if(err)	throw Error("Failed to refresh access token", err);
+
+		return false;
+	}
 }
 
 module.exports = refresh;
