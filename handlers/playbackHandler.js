54ca928e2ffe3c6184e41ebfce544601d952031a
Carl Hallén Jansson <callehj@hotmail.se>
2020-03-04 09:39:25 +0100

    Handle state when nothing is playing but the party has an active status

--- handlers/playbackHandler.js
+++ handlers/playbackHandler.js
@@ -5,15 +5,37 @@ async function playbackHandler (party,
 		progress,
 		listId,
 		currentPlaylist,
-		previousPlaylist
+		deviceId,
+		playbackActive
 	}){
 	const paddingTime = 2000;
-	console.log("remaining", remaining);
 
-	if(!isPlaying && !party.active)
+	if(!deviceId){
+		console.log("No device id was found, cannot play anything.");
+		return;
+	}
+
+	if(!playbackActive && !party.active)
 		return;
 
 	console.log("Trying to play tracks...");
+	if(!isPlaying && party.active){
+		if(currentPlaylist.length > 0)
+			await party.play({
+				uris: currentPlaylist,
+				offset: { uri: currentPlaylist[0] },
+				device_id: deviceId
+			});
+
+		if(currentPlaylist.length === 0)
+			await party.play({
+				context_uri: `spotify:playlist:${listId}`,
+				device_id: deviceId
+			});
+
+		return;
+	}
+
 	if(remaining < paddingTime || progress < paddingTime){
 		if(currentPlaylist.length > 0)
 			await party.play({ uris: currentPlaylist, offset: { uri: currentPlaylist[0] } });
